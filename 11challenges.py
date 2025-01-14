ca547698a61fa586ce747b350a1255ed7a513559
Diwashach30 <diwashacharya30@gmail.com>
2025-01-14 23:42:44 +0545

    11challengecomp

---
+++ 11challenges.py
@@ -0,0 +1,8 @@
+##Write a program that checks if a number is even or odd.
+
+number =int(input("Enter a number :"))
+
+if number % 2 == 0 :
+    print(f"Given Number is Even.")
+else :
+    print(f"Given Number is Odd.")
